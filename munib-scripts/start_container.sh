ddd769870a3b08f7c52a90413b2a1e28456f9f6f
Mirza Munib Baig <102146962+MirzaMunibmmb@users.noreply.github.com>
2024-06-04 18:29:20 +0500

    4-push

--- munib-scripts/start_container.sh
+++ munib-scripts/start_container.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Debug: Print the value of CODEBUILD_BUILD_NUMBER
+echo "CODEBUILD_BUILD_NUMBER: $CODEBUILD_BUILD_NUMBER"
+
 # Pull the Docker image
 docker pull mirzamunibbaig/simple-python-flask-app:$CODEBUILD_BUILD_NUMBER
 
