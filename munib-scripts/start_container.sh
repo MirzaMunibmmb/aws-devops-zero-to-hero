b9232a102f637a0973d7479aaf03c552409d0120
Mirza Munib Baig <102146962+MirzaMunibmmb@users.noreply.github.com>
2024-06-05 15:00:13 +0500

    dasdsa

--- munib-scripts/start_container.sh
+++ munib-scripts/start_container.sh
@@ -6,10 +6,10 @@ set -e
 build_number=$(aws ssm get-parameter --name "/simple-python-app/codebuild/build-number" --query "Parameter.Value" --output text)
 
 # Debug: Print the value of CODEBUILD_BUILD_NUMBER
-echo "CODEBUILD_BUILD_NUMBER: $DOCKER_HUB_ID"
+echo "CODEBUILD_BUILD_NUMBER: $build_number"
 
 # Pull the Docker image
-docker pull mirzamunibbaig/simple-python-flask-app:$DOCKER_HUB_ID
+docker pull mirzamunibbaig/simple-python-flask-app:$build_number
 
 # Run the Docker container
-docker run -d --name simple-python-flask-app -p 6012:5000 mirzamunibbaig/simple-python-flask-app:$latest
+docker run -d --name simple-python-flask-app -p 6012:5000 mirzamunibbaig/simple-python-flask-app:$build_number
