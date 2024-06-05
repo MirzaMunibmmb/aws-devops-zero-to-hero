bc039eeae637b3255234d1a2e2949629112e8986
Mirza Munib Baig <102146962+MirzaMunibmmb@users.noreply.github.com>
2024-06-05 12:12:19 +0500

    sdad

--- munib-scripts/start_container.sh
+++ munib-scripts/start_container.sh
@@ -9,4 +9,4 @@ set -e
 docker pull mirzamunibbaig/simple-python-flask-app:latest
 
 # Run the Docker container
-docker run -d --name simple-python-flask-app -p 8000:5000 mirzamunibbaig/simple-python-flask-app:latest
+docker run -d --name simple-python-flask-app -p 6012:5000 mirzamunibbaig/simple-python-flask-app:latest
