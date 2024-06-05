b6f85b0c2c420437c59254db1bdab3f55b6e063d
Mirza Munib Baig <102146962+MirzaMunibmmb@users.noreply.github.com>
2024-06-05 12:40:56 +0500

    dsadasd

--- munib-scripts/start_container.sh
+++ munib-scripts/start_container.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+env:
+  parameter-store:
+    DOCKER_HUB_ID: /simple-python-app/codebuild/build-number
+
+
 # Debug: Print the value of CODEBUILD_BUILD_NUMBER
-#echo "CODEBUILD_BUILD_NUMBER: latest"
+echo "CODEBUILD_BUILD_NUMBER: $DOCKER_HUB_ID"
 
 # Pull the Docker image
-docker pull mirzamunibbaig/simple-python-flask-app:latest
+docker pull mirzamunibbaig/simple-python-flask-app:$DOCKER_HUB_ID
 
 # Run the Docker container
-docker run -d --name simple-python-flask-app -p 6012:5000 mirzamunibbaig/simple-python-flask-app:latest
+docker run -d --name simple-python-flask-app -p 6012:5000 mirzamunibbaig/simple-python-flask-app:$latest
